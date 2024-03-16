348c4f44aa015a4bff29dbd92b9df492a7a41e9e
Stevens Brito <stevensbrito@gmail.com>
2024-03-16 17:52:54 +0100

    Avoid redundancy by creating one single data adapter class

---
+++ src/domain/repository/adapter.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from src.domain.interfaces.base_repo import Generic, T
+
+
+class DatasetAdapter(Generic[T]):
+
+    def __init__(self, entity: T):
+        self.entity = entity
+
+    def one(self, data) -> T:
+        return self.entity(**data)
+    
+    def many(self, data) -> list[T]:
+        return [self.entity(**items) for items in data]
--- src/domain/repository/project.py
+++ src/domain/repository/project.py
@@ -1,24 +1,14 @@
 from src.domain.entities.project import ProjectEntity
 from src.domain.interfaces.project_repo import IProjectRepository
 from src.infrastructure.db import Db
-from src.domain.interfaces.adapter import IResultSetAdapter
-
-
-class ResultSetAdapter(IResultSetAdapter[ProjectEntity]):
-
-    def single_adapter(self, result_set) -> ProjectEntity:
-        name, key = result_set
-        return ProjectEntity(name, key)
-
-    def list_adapter(self, result_set) -> list[ProjectEntity]:
-        return [ProjectEntity(name, key) for name, key in result_set]
+from src.domain.repository.adapter import DatasetAdapter
 
 
 class ProjectRepository(IProjectRepository):
 
     def __init__(self):
         self.db = Db()
-        self.adapter = ResultSetAdapter
+        self.data_adapter = DatasetAdapter(ProjectEntity)
 
     def create(self, entity: ProjectEntity) -> None:
         key = entity.key.replace(' ', '-').strip()
@@ -32,12 +22,12 @@ class ProjectRepository(IProjectRepository):
     def get_all(self) -> list[ProjectEntity]:
         sql = "SELECT NAME FROM PROJECTS"
         data = self.db.execute(sql)
-        return self.adapter.many(data.fetchall())
+        return self.data_adapter.many(data.fetchall())
     
     def get_one(self, arg: str) -> ProjectEntity:
         sql = f"SELECT NAME FROM PROJECTS WHERE KEY='{arg}'"
         data = self.db.execute(sql)
-        return self.adapter.one(data.fetchone())
+        return self.data_adapter.one(data.fetchone())
     
     def delete(self, arg: str) -> None:
         sql = f"DELETE FROM PROJECTS WHERE KEY='{arg}'"
