06418c8e493fa88282aa54b3048dcd91f7985333
Stevens Brito <stevensbrito@gmail.com>
2024-03-19 02:38:51 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,8 +1,5 @@
-import typer
+from src.application.handlers.cli import app
 
 
-if __name__ == "__main__":
-    
-    from src.application.handlers.cli import app
-    
+if __name__ == "__main__":    
     app()
