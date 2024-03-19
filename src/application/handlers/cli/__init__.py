8b97ef022513bcd9ad89f65d676ad12b9928b5e4
Stevens Brito <stevensbrito@gmail.com>
2024-03-19 02:35:41 +0100

    Change command project -> projects

--- src/application/handlers/cli/__init__.py
+++ src/application/handlers/cli/__init__.py
@@ -5,7 +5,7 @@ import src.application.handlers.cli.project as project
 
 
 app = typer.Typer(name="TODO CLI Application", no_args_is_help=True)
-app.add_typer(project.app, name="project")
+app.add_typer(project.app, name="projects")
 
 
 @app.command(help="Initialize instances for projects. Execute once.")
